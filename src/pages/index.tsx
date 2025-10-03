43515e2a2633c5228c603a0d849a8527bc5987d3
pedroskf <101787482+pedroskf@users.noreply.github.com>
2025-10-03 12:27:17 -0300

    Adiciona página principal index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Header } from '../components/Header';
+import { Hero } from '../components/Hero';
+import { Products } from '../components/Products';
+import { About } from '../components/About';
+import { Contact } from '../components/Contact';
+import { Footer } from '../components/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Products />
+      <About />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
